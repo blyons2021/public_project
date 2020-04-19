ef2faab7d55e838d2bbc586b3dc9f7ec4dbef1d2
blyons2021 <lyonsb1@xavier.edu>
2020-04-19 18:15:49 -0400

    added notifications component

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { MissionComponent } from './mission/mission.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     JobsComponent,
-    MissionComponent
+    MissionComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule
