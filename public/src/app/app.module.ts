e04d9c0a2761853b461276fff44a87da488bc1e9
blyons2021 <lyonsb1@xavier.edu>
2020-04-19 18:13:10 -0400

    updated to add mission component

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { JobsComponent } from './jobs/jobs.component';
+import { MissionComponent } from './mission/mission.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    JobsComponent
+    JobsComponent,
+    MissionComponent
   ],
   imports: [
     BrowserModule
