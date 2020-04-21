6c26fba0204817db25c60f2902de90d671aff0e7
Joshiu <joshiu@xavier.edu>
2020-04-20 22:33:42 -0400

    These are all the changes I have made. There should be a working /jobs, but it's not funcitoning rn, so Ima as Jason why

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { JobsComponent } from './jobs/jobs.component';
@@ -9,6 +9,10 @@ import { NotificationsComponent } from './notifications/notifications.component'
 import { ObjectivesComponent } from './objectives/objectives.component';
 import { ProfileComponent } from './profile/profile.component';
 import { VisionComponent } from './vision/vision.component';
+
+const routes: Routes=[
+  {path: 'jobs', component: JobsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +26,10 @@ import { VisionComponent } from './vision/vision.component';
     VisionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ public/src/app/jobs/job-listing.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'src/environments/environment';
+import { Observable } from 'rxjs';
+const server = environment.server;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobListingService {
+
+  constructor(private http: HttpClient) { }
+  getCompanies(): Observable<any>{
+    return this.http.get<Company[]>(server + '/companies');
+  }
+}
+export interface Company{
+  name: string;
+  primary_phone: string;
+  primary_email: string;
+}
