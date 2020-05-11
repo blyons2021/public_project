b550c9688f25972d83bbe378c95cc08c13ffb92c
Joshiu <joshiu@xavier.edu>
2020-05-11 18:43:20 -0400

    added the Filter for the partner companies!

--- public/src/app/jobs/jobs.component.ts
+++ public/src/app/jobs/jobs.component.ts
@@ -8,14 +8,21 @@ import { JobListingService, Company } from './job-listing.service';
 })
 export class JobsComponent implements OnInit {
   companies: Company[];
+  companiesData: Company[];
   constructor(private readonly jobListingService: JobListingService) { }
 
   ngOnInit(): void {
     this.populate();
   }
+  filter(filterValue: string){
+    this.companiesData = this.companies.filter(c => c.name.toLowerCase().indexOf(filterValue.trim().toLowerCase()) > -1);
+  }
   private populate(){
     this.jobListingService.getCompanies().subscribe(
-      (companies: Company[]) =>this.companies = companies
+      (companies: Company[]) => {
+        this.companies = companies;
+        this.companiesData = companies;
+      }
     )
   }
 }
